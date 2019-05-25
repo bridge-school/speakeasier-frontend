c543a9258365dcb37e3f9e6ca4fd71910d257d91
Leticia Becker <leticiabecker@gmail.com>
2019-05-25 14:34:12 -0400

    Feat/routing (#2)
    * Added routing to form page
    
    * Added routing to header button
    
    * Fixed App rendering
    
    * Fixed App rendering 2
    
    * Removed unused libraries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Form from './Form';
+import HomePage from './HomePage';
 
 
 import './App.css';
@@ -11,20 +9,11 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<Container maxWidth="sm">
-				<Box my={4} display="flex" justifyContent="space-between" alignItems="center">
-					<Typography variant="h4" component="h1" gutterBottom>
-						Speak Easy
-					</Typography>
-
-					<Button color="primary" variant="contained">
-				        Submit Conference
-				    </Button>
-				</Box>
-
-
-			</Container>
-		);
+			<Router>
+				<Route exact path="/" component={HomePage}/>
+				<Route path="/submit-conference" component={Form}/>
+			</Router>
+		)
 	}
 }
 
---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Form extends Component {
+	render() {
+		return (
+			<h1>Form</h1>
+		);
+	}
+}
+
+export default Form;
