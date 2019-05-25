d0e9b3f163222aba18618f884e1b1fc5dab527a3
Joanna <joanna.chan@live.ca>
2019-05-25 13:07:57 -0400

    Added Material UI and header with button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Container maxWidth="sm">
+				<Box my={4} display="flex" justifyContent="space-between" alignItems="center">
+					<Typography variant="h4" component="h1" gutterBottom>
+						Speak Easy
+					</Typography>
+
+					<Button color="primary" variant="contained">
+				        Submit Conference
+				    </Button>
+				</Box>
+
+
+			</Container>
+		);
+	}
 }
 
 export default App;
