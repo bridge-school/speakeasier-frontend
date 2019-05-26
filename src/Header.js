57adf98cc31d182f74498e942e367b9cd6d85a0b
Nayema Nasir <nayema.nasir89@gmail.com>
2019-05-26 18:59:30 -0400

    Destructure props in header component

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
-const Header = (props) =>
+const Header = ({ path, buttonText }) =>
   <Container>
     <Box my={4} display="flex" justifyContent="space-between" alignItems="center">
       <Typography variant="h4" component="h1" gutterBottom>
@@ -17,9 +17,9 @@ const Header = (props) =>
         color="primary"
         variant="contained"
         component={Link}
-        to={props.path}
+        to={path}
       >
-        {props.buttonText}
+        {buttonText}
       </Button>
     </Box>
   </Container>;
