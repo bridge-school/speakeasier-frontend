a0e625c72e1a79db5789c9fcea23f4fa5f813c57
Nayema Nasir <nayema.nasir89@gmail.com>
2019-05-25 22:44:22 -0400

    Add font to home page title

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,28 +3,30 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 class HomePage extends Component {
-	render() {
-		return (
-			<Container maxWidth="sm">
-				<Box my={4} display="flex" justifyContent="space-between" alignItems="center">
-					<Typography variant="h4" component="h1" gutterBottom>
-						Speak Easy
-					</Typography>
-					<Button 
-						color="primary" 
-						variant="contained" 
-						component={Link}
-						to="/submit-conference"
-					>
-				        Submit Conference
-				    </Button>
-				</Box>
-			</Container>
-		);
-	}
+  render () {
+    return (
+      <Container maxWidth="sm">
+        <Box my={4} display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h4" component="h1" gutterBottom>
+            <Box fontFamily="Monospace">
+              Speak Easier
+            </Box>
+          </Typography>
+          <Button
+            color="primary"
+            variant="contained"
+            component={Link}
+            to="/submit-conference"
+          >
+            Submit Conference
+          </Button>
+        </Box>
+      </Container>
+    );
+  }
 }
 
 export default HomePage;
