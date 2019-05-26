cc1ce21a331513a128d5555f96dfffc63b9a71cd
Vanessa Mendoza <vanecaro24@gmail.com>
2019-05-26 13:53:01 -0400

    Added event and submission date picker using material ui component pickers (#5)

--- src/FormPage.js
+++ src/FormPage.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
+import Moment from '@date-io/moment';
+import {
+  DatePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
 
-const FormPage = () =>
-  <Container maxWidth="md">
-    <Box my={4} display="flex" justifyContent="space-between" alignItems="center">
-      <Typography variant="h4" component="h1" gutterBottom>
-        <Box fontFamily="Monospace">
-          CONFERENCE FORM
-        </Box>
-      </Typography>
-      <Button
-        color="primary"
-        variant="contained"
-        component={Link}
-        to="/"
-      >
-        Back To Conferences
-      </Button>
-    </Box>
-  </Container>;
+const FormPage = () => {
+  const [selectedEventDate, setEventDate] = useState(new Date());
+  const [selectedSubmissionDate, setSubmissionDate] = useState(new Date());
+
+  const onEventDateChange = (date) => {
+    setEventDate(date)
+  }
+
+  const onSubmissionDateChange = (date) => {
+    setSubmissionDate(date)
+  }
+
+  return (
+    <Container maxWidth="md">
+      <Box my={4} display="flex" justifyContent="space-between" alignItems="center">
+        <Typography variant="h4" component="h1" gutterBottom>
+          <Box fontFamily="Monospace">
+            CONFERENCE FORM
+          </Box>
+        </Typography>
+        <Button
+          color="primary"
+          variant="contained"
+          component={Link}
+          to="/"
+        >
+          Back To Conferences
+        </Button>
+      </Box>
+
+      <MuiPickersUtilsProvider utils={Moment}>
+        <h4>Event Date</h4>
+        <DatePicker value={selectedEventDate} onChange={onEventDateChange} />
+      </MuiPickersUtilsProvider>
+
+      <MuiPickersUtilsProvider utils={Moment}>
+        <h4>Submission Date</h4>
+        <DatePicker value={selectedSubmissionDate} onChange={onSubmissionDateChange} />
+      </MuiPickersUtilsProvider>
+    </Container>
+  )
+}
 
 
 export default FormPage;
