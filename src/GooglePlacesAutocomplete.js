4353aafe5f1c603b51eb73b8afbe7f043cd3fb18
Joanna <joanna.chan@live.ca>
2019-05-27 21:32:39 -0400

    Moved script loading placement to index.html file for better ease-of-use.

--- src/GooglePlacesAutocomplete.js
+++ src/GooglePlacesAutocomplete.js
@@ -9,7 +9,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-const gmapsApi = `AIzaSyCNGK9oXaPsMZFDfz-h_74IR1-qpnYxo5I`;
 
 class GooglePlacesAutocomplete extends Component {
 
@@ -30,69 +29,19 @@ class GooglePlacesAutocomplete extends Component {
 		this.setState({ address });
 	};
 
-
-	initMap = () => {
-		this.setState({ gmapsLoaded: true });
-	}
-
 	handleError = (status, clearSuggestions) => {
 	  console.log('Google Maps API returned error with status: ', status);
 	  clearSuggestions();
 	}
 
-	componentDidMount() {
-		window.initMap = this.initMap;
 
-		const gmapScriptEl = document.createElement(`script`)
-		gmapScriptEl.src = `https://maps.googleapis.com/maps/api/js?key=${gmapsApi}&libraries=places&callback=initMap`;
 
-		document.querySelector(`body`).insertAdjacentElement(`beforeend`, gmapScriptEl);
 	}
 
 	render() {
 
 		return (
-			<div>
-			{this.state.gmapsLoaded &&
-				<PlacesAutocomplete
-					value={this.state.address}
-					onChange={this.handleChange}
-					onSelect={this.handleSelect}
-					onError={this.handleError}
-					googleCallbackName="initMap"
-				>
-					{({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-						<div>
-							<InputLabel>
-								Event Location
-							</InputLabel>
-
-							<Input className={this.props.className}
-								{...getInputProps({
-									placeholder: ''
-								})}
-							/>
-
-							{loading && <p>Loading...</p>}
-
-							{(!loading && suggestions.length > 0) &&
-								<List>
-									{suggestions.map(suggestion => {
-										return (
-											<ListItem button
 											{...getSuggestionItemProps(suggestion)}
-											>
-											  <ListItemText primary={suggestion.description} />
-											</ListItem>
-										);
-									})}
-								</List>
-							}
-						</div>
-					)}
-				</PlacesAutocomplete>
-			}
-			</div>
 		)
 	}
 }
