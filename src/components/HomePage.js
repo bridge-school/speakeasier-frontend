58e3e28597981901ebe37733408294fe00241dba
Nayema Nasir <nayema.nasir89@gmail.com>
2019-05-31 21:54:23 -0400

    Refactor using hooks

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Header from './Header';
 
-class HomePage extends Component {
-	render() {
-		const { events } = this.props;
-		return (
-		  <>
-		    <Header buttonText="Submit Conference" path="/submit-conference"/>
-		    <Container maxWidth="md">
-		      <Box flexGrow={1} textAlign="center">
-		        <h1>Home Page</h1>
-						<p>{ events.length > 0 && events.map(event => event.name) }</p>
-		      </Box>
-		    </Container>
-		  </>
-		)
-	}
+const HomePage = ({ events, getConferences }) => {
+
+  useEffect(() => {
+    getConferences();
+  }, [getConferences]);
+
+  return (
+    <>
+      <Header buttonText="Submit Conference" path="/submit-conference"/>
+      <Container maxWidth="md">
+        <Box flexGrow={1} textAlign="center">
+          <h1>Home Page</h1>
+          <p>{events.length > 0 && events.map(event => event.name)}</p>
+        </Box>
+      </Container>
+    </>
+  );
 };
-
-
 
 export default HomePage;
--- src/containers/HomePageCtn.js
+++ src/containers/HomePageCtn.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
 import { connect } from "react-redux";
 import HomePage from "../components/HomePage";
 import { getConferences } from "../actions/actions";
 
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {
     events: state.events
   };
@@ -13,17 +12,9 @@ const mapDispatchToProps = {
   getConferences
 };
 
-class HomePageCtn extends Component {
-	componentDidMount() {
-		this.props.getConferences();
-	}
-
-	render() {
-		return <HomePage events={this.props.events} />
-	}
-}
-
-export default connect(
+const HomePageCtn  = connect(
   mapStateToProps,
   mapDispatchToProps
-)(HomePageCtn);
+)(HomePage);
+
+export default HomePageCtn;
