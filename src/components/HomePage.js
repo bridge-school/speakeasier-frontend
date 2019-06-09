8edddb167f63a83469c70953035d38df3b309aca
Vanessa Mendoza <vanessamendoza@Vanessas-MacBook-Pro.local>
2019-06-09 12:07:30 -0400

    Changed styling to home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,20 +5,34 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 import Header from "./Header";
 import EventList from "./EventList";
 import SearchBar from './SearchBar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    fontWeight: '300',
+    color: '#181818',
+    display: 'flex',
+    justifyContent: 'left',
+    margin: '10px'
+  }
+}));
 
 const HomePage = ({ events, isLoading, error, getEvents, queryForEvents, searchQuery }) => {
   useEffect(() => {
     getEvents();
   }, [getEvents]);
 
+  const classes = useStyles();
+
   return (
     <div>
       <Header buttonText="Submit Conference" path="/submit-conference" />
       <Container maxWidth="md">
         <Box flexGrow={1} textAlign="center">
+          <h1 className={classes.title}>Upcoming events</h1>
+					<SearchBar queryForEvents={queryForEvents} />
           {isLoading && <LinearProgress />}
           {error && <div>Unable to fetch events. Please try again</div>}
-					<SearchBar queryForEvents={queryForEvents} />
           <EventList events={events} searchQuery={searchQuery} />
         </Box>
       </Container>
