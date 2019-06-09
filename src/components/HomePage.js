7af565d393a8900b62fbf4a57d095f781de740f3
Vanessa Mendoza <vanessamendoza@Vanessas-MacBook-Pro.local>
2019-06-09 12:21:41 -0400

    Added margin bottom at the end of the list

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Header from "./Header";
+import React, { useEffect } from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Header from './Header';
 import EventList from "./EventList";
 import SearchBar from './SearchBar';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
+  box: {
+    marginBottom: '80px'
+  },
   title: {
     fontWeight: '300',
     color: '#181818',
@@ -28,7 +31,7 @@ const HomePage = ({ events, isLoading, error, getEvents, queryForEvents, searchQ
     <div>
       <Header buttonText="Submit Conference" path="/submit-conference" />
       <Container maxWidth="md">
-        <Box flexGrow={1} textAlign="center">
+        <Box flexGrow={1} textAlign="center" className={classes.box}>
           <h1 className={classes.title}>Upcoming events</h1>
 					<SearchBar queryForEvents={queryForEvents} />
           {isLoading && <LinearProgress />}
