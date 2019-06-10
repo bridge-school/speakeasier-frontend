026369643278891a54cb703f22d6d3ab6ab31014
Nayema Nasir <nayema.nasir89@gmail.com>
2019-06-10 08:02:46 -0400

    Replace values with separate value fields

--- src/components/Form/ContactSection.js
+++ src/components/Form/ContactSection.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
-const ContactSection = ({ values, classInput, inputOnChange, inputError }) => {
-  return (
+const ContactSection = ({
+    contactName,
+    contactEmail,
+    classInput,
+    inputOnChange,
+    inputError
+  }) =>
+  (
     <>
-    <TextField
-      id="standard-name"
-      label="Contact Name"
-      placeholder="Contact Name"
-      name="contactName"
-      value={values.contactName}
-      onChange={inputOnChange}
-      className={classInput}
-    />
-    <TextField
-      id="standard-name"
-      label="Contact E-mail"
-      placeholder="Contact Email"
-      name="contactEmail"
-      value={values.contactEmail}
-      onChange={inputOnChange}
-      className={classInput}
-      error={inputError}
-    />
+      <TextField
+        id="standard-name"
+        label="Contact Name"
+        placeholder="Contact Name"
+        name="contactName"
+        value={contactName}
+        onChange={inputOnChange}
+        className={classInput}
+      />
+      <TextField
+        id="standard-name"
+        label="Contact E-mail"
+        placeholder="Contact Email"
+        name="contactEmail"
+        value={contactEmail}
+        onChange={inputOnChange}
+        className={classInput}
+        error={inputError}
+      />
     </>
-  )
-}
+  );
 
 export default ContactSection;
