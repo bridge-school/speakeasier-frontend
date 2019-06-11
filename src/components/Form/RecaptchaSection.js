d068a48f1b0eb9ec780efb60a1e1cf6bc40b112e
Leticia Becker <leticiabecker@gmail.com>
2019-06-10 23:33:28 -0400

    Recaptcha comp refactored with hooks

--- src/components/Form/RecaptchaSection.js
+++ src/components/Form/RecaptchaSection.js
@@ -1,36 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { ReCaptcha } from "react-recaptcha-google";
 
-class RecaptchaSection extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
-  }
-  componentDidMount() {
-    if (this.formCaptcha) {
-      this.formCaptcha.reset();
+const RecaptchaSection = () => {
+
+  useEffect(() => {
+    if (formCaptcha) {
+      formCaptcha.reset();
     }
-  }
-  onLoadRecaptcha() {
-    if (this.formCaptcha) {
-      this.formCaptcha.reset();
+  }, []);
+
+  const onLoadRecaptcha = () => {
+    if (formCaptcha) {
+      formCaptcha.reset();
     }
   }
 
-  render() {
-    return (
+  let formCaptcha;
+  
+  return (
       <div>
         <ReCaptcha
-          ref={el => {
-            this.formCaptcha = el;
-          }}
+          ref={(el) => {formCaptcha = el}}
           size="normal"
           render="explicit"
           sitekey="6LcZKagUAAAAAP0TiXY27vKeN83TGm3tvkIMN8Ps"
-          onloadCallback={this.onLoadRecaptcha}
+          onloadCallback={onLoadRecaptcha}
         />
       </div>
     );
-  }
 }
+
 export default RecaptchaSection;
