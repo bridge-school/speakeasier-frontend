4db447025d9b21bf7e81908a28ea1931ee9e5ca3
Joanna <joanna.chan@live.ca>
2019-06-16 15:13:59 -0400

    Added an Event Details page and the ability to route to the Event Details page using the Event ID.

---
+++ src/components/EventDetails/EventDetailsPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Header from '../Events/Header';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    box: {
+        marginBottom: '80px'
+    },
+    title: {
+        fontWeight: '300',
+        color: '#181818',
+        display: 'flex',
+        justifyContent: 'left',
+        margin: '10px'
+    }
+});
+
+const EventDetailsPage = ({ match }) => {
+
+    const classes = useStyles();
+
+    const eventId = match.params.id;
+
+    return (
+        <div>
+            <Header buttonText="Submit Conference" path="/submit-conference" />
+
+            <Container maxWidth="md">
+                <Box flexGrow={1} textAlign="center" className={classes.box}>
+                    <h1 className={classes.title}>Conference: {eventId}</h1>
+                </Box>
+            </Container>
+        </div>
+    );
+};
+
+export default EventDetailsPage;
--- src/components/Events/EventList.js
+++ src/components/Events/EventList.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import EventListItem from "./EventListItem";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1
+  },
+  linkStyle: {
+    textDecoration: "none"
   }
 });
 
-const EventList = ({ events, test, searchQuery }) => {
+const EventList = ({ events, searchQuery }) => {
   const classes = useStyles();
 
 	const filteredEvents = events.filter(event => event.eventName.toLowerCase().indexOf(searchQuery.toLowerCase()) > -1);
 
   return (
     <div className={classes.root}>
-      {filteredEvents.map(event => (
-        <EventListItem event={event} key={event.id}/>
-      ))}
-			{filteredEvents.length === 0 && <p>No events found.</p>}
+        {filteredEvents.map(event => (
+          <Link to={"/conference/" + event.id} key={event.id} className={classes.linkStyle}>
+             <EventListItem event={event}/>
+          </Link>
+        ))}
+      
+      {filteredEvents.length === 0 && <p>No events found.</p>}
+  
     </div>
   );
 };
