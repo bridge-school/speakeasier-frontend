30d22766556d7e6622398833c018a7215a65f192
Joanna Chan <joanna.chan@live.ca>
2019-06-16 20:28:32 -0400

    Event Details Route (#40)
    * Added an Event Details page and the ability to route to the Event Details page using the Event ID.
    
    * Added link on Logo to easily route back to the home page when on the Details Page.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import FormPage from './Form/FormPage';
 import EventPageCtn from '../containers/EventPageCtn';
+import EventDetailsPage from './EventDetails/EventDetailsPage';
 
 const App = () =>
   <Router>
     <Switch>
+      <Route path="/" exact component={EventPageCtn}/>
       <Route path="/submit-conference" component={FormPage}/>
-      <Route path="/" component={EventPageCtn}/>
+      <Route path="/conference/:id" component={EventDetailsPage} />
     </Switch>
   </Router>;
 
